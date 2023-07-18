b7a016016d88ec8cebf35a4f9b1d2eccb2ff1d28
mjgomsa-lippincott <mj.gomez-saavedra@lippincott.com>
2023-07-18 09:17:17 -0400

    Quest2 updates working

--- 00_Lip_FlightTrack/Assets/Scripts/LoadingScripts/HandPresence.cs
+++ 00_Lip_FlightTrack/Assets/Scripts/LoadingScripts/HandPresence.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
+
 public class HandPresence : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
+        
+
         InputDevices.GetDevices(devices);
 
-        foreach(var device in devices)
+        foreach(var item in devices)
         {
-            Debug.Log(device.name + device.characteristics);
+            Debug.Log(item.name + item.characteristics);
         }
-        
+
+
     }
 
     // Update is called once per frame
